228aa1f5682db3050c4534f4f97775222318e4f6
Tariq Magsi <tariqmagsi125@gmail.com>
2019-09-07 21:00:45 +0500

    untracked node_modules

--- online-quran/src/components/Login.js
+++ online-quran/src/components/Login.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { getFromStorage } from "../utils/storage";
 import "../css/Formstyle.css";
 import LoginForm from "./Loginform";
+import { fromJSON } from "tough-cookie";
 
 class Signin extends Component {
+  state = { issAdmin: false };
+  componentDidMount() {
+    const obj = getFromStorage(process.env.REACT_APP_KEY);
+
+    if (!(obj && obj.token)) {
+      this.setState({
+        issAdmin: true
+      });
+    }
+  }
   render() {
-    return (
+    return this.state.issAdmin ? (
       <div className="backSection">
         <div className="signin-section">
           <div className="container">
@@ -12,7 +25,9 @@ class Signin extends Component {
           </div>
         </div>
       </div>
+    ) : (
+      <div />
     );
   }
 }
-export default Signin;
+export default withRouter(Signin);
