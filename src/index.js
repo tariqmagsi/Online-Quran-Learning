f3cd4f2dce99c0d1d621750d83cb576b1fdf3d55
Tariq Magsi <tariqmagsi125@gmail.com>
2019-09-07 15:55:24 +0500

    untracked node_modules

--- src/index.js
+++ src/index.js
@@ -17,9 +17,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("online-quran/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "online-quran", "build", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname + "/../online-quran/build/index.html"));
   });
 }
 
